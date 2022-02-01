0ceab295bc408edd1bc75a00a4bcfbcf6bb9ea7a
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-02-01 21:22:21 +0530

    data from live api

--- src/components/Coursedisplay.js
+++ src/components/Coursedisplay.js
@@ -1,28 +1,41 @@
 import React, { Component } from "react";
-
 export default class Coursedisplay extends Component {
-  constructor(props) {
-    super(props);
-  }
   rendercourses = (data) => {
-    console.log("data in course display>>>", data);
-    return data.map((item) => {
-      console.log("item is >>>", item);
-      return (
-        <div className="course-info">
-          <div className="course-img">
-            <img src={item.thumb} alt="course" />
+    if (data) {
+      return data.map((item) => {
+        return (
+          <div className="course-info" key={item._id}>
+            <div className="course-img">
+              <img src={item.thumb} alt="course" />
+            </div>
+            <div className="course-desc">
+              <h2>{item.name}</h2>
+              <p className="desc-text">{item.description}</p>
+            </div>
+            <button className="btn btn-danger" value={item.id}>
+              Register here
+            </button>
           </div>
-          <div className="course-desc">
-            <h2>{item.name}</h2>
-            <p className="desc-text">{item.description}</p>
-            <button className="btn btn-danger">Register here</button>
-          </div>
-        </div>
-      );
-    });
+        );
+      });
+    }
   };
   render() {
-    return <>{this.rendercourses(this.props.data)}</>;
+    return (
+      <>
+        <div className="container">
+          <div className="panel panel-success">
+            <div className="panel-heading">
+              <h4>Register your courses</h4>
+            </div>
+            <div className="panel-body">
+              <div className="all-courses">
+                {this.rendercourses(this.props.coursedata)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,34 +1,30 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import "./courses.css";
-import JSON from "./db.json";
 import Coursedisplay from "./Coursedisplay";
-
+//api url for courses
+const url = "http://localhost:5000/courses";
 class Courses extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      coursedata: JSON,
+      coursedata: "",
     };
   }
   render() {
     return (
       <div className="courses">
         <Header />
-        <div className="container">
-          <div className="panel panel-success">
-            <div className="panel-heading">
-              <h4>Register your courses</h4>
-            </div>
-            <div className="panel-body">
-              <div className="all-courses">
-                <Coursedisplay data={this.state.coursedata} />
-              </div>
-            </div>
-          </div>
-        </div>
+        <Coursedisplay coursedata={this.state.coursedata} />
       </div>
     );
+  }
+  componentWillMount() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ coursedata: data });
+      });
   }
 }
 export default Courses;
